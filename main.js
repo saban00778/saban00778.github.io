a9fc6662d60b34f414789a90d6345e2b4e51e31a
saban00778 <89841157+saban00778@users.noreply.github.com>
2021-10-06 21:27:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,29 +18,27 @@ easy.addEventListener("click", () => {
     let num2 = Math.round(Math.random() * 150);
     player.style.marginTop = `${num1}px`;
     player.style.marginLeft = `${num2}px`;
-    sumarPuntos();
   });
 });
 
 normal.addEventListener("click", () => {
+  player.classList.remove("animation");
   player.addEventListener("mouseover", () => {
     let num1 = Math.round(Math.random() * 500);
     let num2 = Math.round(Math.random() * 500);
     player.style.marginTop = `${num1}px`;
     player.style.marginLeft = `${num2}px`;
-    sumarPuntos();
   });
 });
 
 difficult.addEventListener("click", () => {
+  player.classList.add("animation");
   player.addEventListener("mouseover", () => {
     let num1 = Math.round(Math.random() * 500);
     let num2 = Math.round(Math.random() * 500);
     player.style.marginTop = `${num1}px`;
     player.style.marginLeft = `${num2}px`;
-    sumarPuntos();
   });
-  player.classList.add("animation");
 });
 
 imposible.addEventListener("click", () => {
@@ -49,4 +47,8 @@ imposible.addEventListener("click", () => {
     player.style.display = "none";
     alert("Jaja como te lo vas a pasar si dice imposible");
   });
+});
+
+player.addEventListener("mouseover", () => {
+  sumarPuntos();
 });
